0c20c2c4b1d882b9864a3a32c35589475a83062c
software-1234 <irishswedens@gmail.com>
2017-10-27 22:26:00 -0400

    made changes to app.py and addded basic twilio funcitonality

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 from flask import json,Flask,render_template,request,jsonify
+from twilio.twiml.messaging_response import MessagingResponse, Message
+#from twilio.rest import Client
+#from twilio import twiml
+#import message_maker
+# Your Account SID from twilio.com/console
+account_sid = "AC48bfb30988e4421136472e5e02927a37"
+# Your Auth Token from twilio.com/console
+auth_token  = "8c6d3bc77693b473e644d6438c8b5bb"
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,3 +21,16 @@ def profiles():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/sms', methods=['POST'])
+def text():
+    print('received')
+    # Get the text in the message sent
+    response = MessagingResponse()
+    #message_body = request.form['Body']
+    #print(message_body)   
+    # Create a Twilio response object to be able to send a reply back (as per         # Twilio docs)
+    response.message('hi, aaron')
+    print('hi')
+    return str(response)
+
